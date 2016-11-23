df5843ae948cfc720a95483a6b6b2274671db55d
Grimson <matt9796@gmail.com>
2016-11-23 16:38:37 -0600

    Debuging commit/push script

--- ORR-ME_GitCommit.sh
+++ ORR-ME_GitCommit.sh
@@ -3,5 +3,5 @@ cp -r /home/matthew/Eclipse/Projects/ORR-ME /home/matthew/git/
 cd /home/matthew/git/ORR-ME
 git add *
 git commit
-$1
+echo $1
 git push
--- ORR-ME_GitCommit.sh~
+++ ORR-ME_GitCommit.sh~
@@ -3,4 +3,5 @@ cp -r /home/matthew/Eclipse/Projects/ORR-ME /home/matthew/git/
 cd /home/matthew/git/ORR-ME
 git add *
 git commit
+$1
 git push
