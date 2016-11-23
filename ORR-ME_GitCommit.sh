186a1ec63151dcf3d9c897142881d01f4083855c
Grimson <matt9796@gmail.com>
2016-11-23 16:44:03 -0600

    Debuging a function to only commit and push if wanted.

--- ORR-ME_GitCommit.sh
+++ ORR-ME_GitCommit.sh
@@ -2,6 +2,10 @@
 cp -r /home/matthew/Eclipse/Projects/ORR-ME /home/matthew/git/
 cd /home/matthew/git/ORR-ME
 git add *
+git status
+$changes  git status | grep committed
+$changes
+read
 git commit
 echo $1
 git push
