dcc377a4e38433b4b26071c651cffc1d2bd733fd
Grimson <matt9796@gmail.com>
2016-11-23 21:39:19 -0600

    ReAdding this file after mastakingly wipeing it from my hard drive.

--- ORR-ME_GitCommit.sh
+++ ORR-ME_GitCommit.sh
@@ -2,19 +2,22 @@
 changedSuffex="anges to be committed:"
 unchangedSuffex="thing to commit, working directory *"
 
-
+############################# Commit Function #############################
+#  This block copies my working directory to the git directory.  Then adds all new files.
+#	 Checks the old files for changes before running commit command.  Then assuming all 
+#  goes well will ask you if you want to push your project as well.
 function commitBlock () {
 cp -r /home/matthew/Eclipse/Projects/ORR-ME /home/matthew/git/
 cd /home/matthew/git/ORR-ME
 git add *
-git status
+#git status
 changes=$(git status | grep -w -e nothing -e committed)
 changes=${changes%$changedSuffex}
 changes=${changes%%$unchangedSuffex}
-echo "$changes"
+#echo "$changes"
 case "$changes" in
 	'no')
-			printf "There are no changes to commit."
+			printf "There are no changes to commit.\n"
 			input
 			;;
 	'Ch')
@@ -44,9 +47,16 @@ esac
 
 }
 
+############################ Pull Function ####################################
+
+
+function pullBlock() {
+	
+}
+
 function input () {
 #Asks what you want it to do and then will direct you to the correct block of code to preform that function.
-read -p "Would you like to Push, Pull, or Commit your Project?"
+read -p "Would you like to Push, Pull, or Commit your Project? (Hint: exit will end program)"
 currentAction=$REPLY
 echo "test"
 echo "$currentAction"
@@ -65,6 +75,8 @@ case "$currentAction" in
 			;&
 	"auto")
 			autoRun
+			;;
+	"exit")
 			;;
 	*)
 			echo "Please try that input again!"
