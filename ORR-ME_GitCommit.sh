95a04e700a107b41b7d316c4c00a5ff1b7b2cb77
Grimson <matt9796@gmail.com>
2016-11-23 21:15:42 -0600

    Debuging Script Still.  Finished detecting changes and now adding menu and Push, Pull, Commit, and Auto Functions.  This is fun.

--- ORR-ME_GitCommit.sh
+++ ORR-ME_GitCommit.sh
@@ -1,27 +1,80 @@
 #!/bin/bash
 changedSuffex="anges to be committed:"
 unchangedSuffex="thing to commit, working directory *"
+
+
+function commitBlock () {
 cp -r /home/matthew/Eclipse/Projects/ORR-ME /home/matthew/git/
 cd /home/matthew/git/ORR-ME
 git add *
 git status
 changes=$(git status | grep -w -e nothing -e committed)
-echo "$changes"
 changes=${changes%$changedSuffex}
 changes=${changes%%$unchangedSuffex}
-echo "${changes}"
+echo "$changes"
+case "$changes" in
+	'no')
+			printf "There are no changes to commit."
+			read
+			;;
+	'Ch')
+			printf "Changes found, Starting commit process.\n"
+			git commit
+			read -p "Would you like to continue to Push? (y=yes, n=exit)"
+			newinput=$REPLY
+			case "$newinput" in
+				'y')
+						pushBlock
+						;;
+				'n')
+						exit
+						;;
+				*)
+						printf "Error Incorect input.  Good Bye."
+						exit
+						;;
+			esac
+			;;
+	*)
+			printf "Critical Error Encountered when parsing changes.  Press any key to exit>>"
+			read
+			exit
+			;;
+esac
+
+}
+
+function input () {
+#Asks what you want it to do and then will direct you to the correct block of code to preform that function.
+read -p "Would you like to Push, Pull, or Commit your Project?"
+currentAction=$REPLY
+echo "test"
+echo "$currentAction"
+case "$currentAction" in
+	'push')
+			pushBlock
+			;;
+	"pull")
+			pullBlock
+			;;
+	"commit")
+			commitBlock
+			;;
+	"a")
+			autoRun
+			;&
+	"auto")
+			autoRun
+			;;
+	*)
+			echo "Please try that input again!"
+			input
+			;;
+esac
+printf "Have a Nice Day.  Press Any Key to Exit>>"
 read
-git commit
-echo $1
-git push
+exit
+}
 
-#Your branch is ahead of 'origin/master' by 5 commits.
-#  (use "git push" to publish your local commits)
-#nothing to commit, working directory clean
-
-#Your branch is ahead of 'origin/master' by 4 commits.
-#  (use "git push" to publish your local commits)
-#Changes to be committed:
-
-
-
+####################Where Program Starts############################
+input
